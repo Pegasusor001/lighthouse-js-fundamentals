d6b4f8c59dc73d74752cb1151168d50f7ff2c751
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-11 02:05:30 +0000

    whichschool-test

---
+++ whichschool.js
@@ -0,0 +1,15 @@
+function whichschool(age) {
+  if (age < 13) {
+    console.log('Elementary School')
+  } else if (age >=13 && age<=18) {
+      console.log('Secondary School')
+    } else {
+      console.log('Lighthouse Labs')
+  }
+}
+
+whichschool (15)
+
+
+
+
