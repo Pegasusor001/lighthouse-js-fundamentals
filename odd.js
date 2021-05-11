d5af68055e26077a5fa67732e0e6ab837dc93252
Pegasusor001 <linus.ji.001@gmail.com>
2021-05-11 02:11:14 +0000

    odd-test

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd  = function (num) {
+  return num % 2 === 1;
+}
+
+const threeIsEven = isEven(3);
+const eightIsEven = isEven(8);
+
+console.log('3 is odd ' + threeIsOdd);
+console.log('8 is odd ' + eightIsOdd);
